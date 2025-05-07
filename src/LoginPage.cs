7404149470f6812c77477e220fc209dc4e2de5e2
Tomasz Lesniak <tomasz.lesniak1@pl.abb.com>
2025-05-07 13:48:56 +0200

    Setup

---
+++ src/LoginPage.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+
+namespace Abb.Tests;
+
+public class LoginPage(IWebDriver webDriver)
+{
+    public void Login(string userName, string password)
+    {
+    }
+}
---
+++ src/LoginTests.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using Xunit;
+
+namespace Abb.Tests;
+
+public class LoginTests(IWebDriver webDriver)
+{
+    [Fact]
+    public void TestChangeUserName()
+    {
+        webDriver.FindElement(By.Id("profile-link")).Click();
+        webDriver.FindElement(By.Id("name")).Clear();
+        webDriver.FindElement(By.Id("name")).SendKeys("Janek");
+        string currentValue = webDriver.FindElement(By.Id("name")).GetAttribute("value");
+        Assert.Equal("Janek", currentValue);
+        webDriver.FindElement(By.Id("save-profile")).Click();
+        string message = webDriver.FindElement(By.Id("success-msg")).Text;
+        Assert.Equal("Profil został zaktualizowany", message);
+    }
+}
